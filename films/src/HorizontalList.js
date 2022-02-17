805466e14af89e64f0a34513da3e8acb4053db19
pbernard006 <pbernard006@iutbayonne.univ-pau.fr>
2022-02-17 16:30:45 +0100

    Bugfix HorizontalList

--- films/src/HorizontalList.js
+++ films/src/HorizontalList.js
@@ -7,7 +7,7 @@ function HorizontalList({data}){
             {data.slice(0,3).map((entry) => (
                 <div key={entry.id} className="col">
                     <Link to={`/movies/${entry.id}`}>
-                        <img src={`https://image.tmdb.org/t/p/w92${entry.backdrop_path}`}></img>
+                        <img src={`https://image.tmdb.org/t/p/w92${entry.poster_path}`}></img>
                     </Link>
                 </div>
             ))}
