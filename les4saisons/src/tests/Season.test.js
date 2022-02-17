e94dfc0e12f03d0ce53c8a78ceb86d54cf820acb
pbernard006 <pbernard006@iutbayonne.univ-pau.fr>
2022-02-17 09:39:40 +0100

    Tests pas encore fonctionnels

--- les4saisons/src/tests/Season.test.js
+++ les4saisons/src/tests/Season.test.js
@@ -5,7 +5,10 @@ import { render, screen} from '@testing-library/react' ;
 import "@testing-library/jest-dom" ;
 
 describe(Season.name, () => {
-    it("renders a title 'Winter'", () => {});
+    it("renders a title 'Winter'", () => {
+        render(<Season name="Winter"/>);
+        expect(screen.queryByText("Winter")).toBeInTheDocument();
+    });
     it("renders a start date '2021-12-21'", () => {});
     it("renders a end date '2022-03-19", () => {});
 })
